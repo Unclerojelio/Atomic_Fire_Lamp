a1455abc6527e830d279667396034171a91c3ce1
Roger Banks <roger_banks@mac.com>
2021-07-26 20:41:29 -0500

    First light

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include <Arduino.h>
 
-// the setup function runs once when you press reset or power the board
+// MultipleStripsInOneArray - see https://github.com/FastLED/FastLED/wiki/Multiple-Controller-Examples for more info on
+// using multiple controllers.  In this example, we're going to set up four NEOPIXEL strips on three
+// different pins, each strip will be referring to a different part of the single led array
+
+#include <FastLED.h>
+
+#define NUM_STRIPS 4
+#define NUM_LEDS_PER_STRIP 52
+#define NUM_LEDS NUM_LEDS_PER_STRIP * NUM_STRIPS
+
+CRGB leds[NUM_STRIPS * NUM_LEDS_PER_STRIP];
+
+// For mirroring strips, all the "special" stuff happens just in setup.  We
+// just addLeds multiple times, once for each strip
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // tell FastLED there's 60 NEOPIXEL leds on pin D1, starting at index 0 in the led array
+  FastLED.addLeds<NEOPIXEL, D1>(leds, 0, NUM_LEDS_PER_STRIP);
+
+  // tell FastLED there's 60 NEOPIXEL leds on pin D2, starting at index 60 in the led array
+  FastLED.addLeds<NEOPIXEL, D2>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
+  // tell FastLED there's 60 NEOPIXEL leds on pin D3, starting at index 120 in the led array
+  FastLED.addLeds<NEOPIXEL, D3>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
+    // tell FastLED there's 60 NEOPIXEL leds on pin D4, starting at index 120 in the led array
+  FastLED.addLeds<NEOPIXEL, D4>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                       // wait for a second
+  for(int i = 0; i < NUM_LEDS_PER_STRIP; i++) {
+    leds[i] = CRGB::Red;
+    leds[i +     NUM_LEDS_PER_STRIP] = CRGB::Red;
+    leds[i + 2 * NUM_LEDS_PER_STRIP] = CRGB::Red;
+    leds[i + 3 * NUM_LEDS_PER_STRIP] = CRGB::Red;
+    FastLED.show();
+    leds[i] = CRGB::Black;
+    leds[i +     NUM_LEDS_PER_STRIP] = CRGB::Black;
+    leds[i + 2 * NUM_LEDS_PER_STRIP] = CRGB::Black;
+    leds[i + 3 * NUM_LEDS_PER_STRIP] = CRGB::Black;
+    delay(50);
+  }
 }
