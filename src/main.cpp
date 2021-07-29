93c21a2ae1908ca198086557e6da03b4286e4643
Roger Banks <roger_banks@mac.com>
2021-07-28 19:49:59 -0500

    Remove useless comments.

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ CRGB arm2Leds[NUM_LEDS_PER_STRIP];
 CRGB arm3Leds[NUM_LEDS_PER_STRIP];
 CRGB arm4Leds[NUM_LEDS_PER_STRIP];
 
-ClassicFireEffect fire(NUM_LEDS_PER_STRIP, 20, 100, 3, 4, true, false);     // Outwards from Zero
+//ClassicFireEffect fire(NUM_LEDS_PER_STRIP, 20, 100, 3, 4, true, false);     // Outwards from Zero
 
 
 // For mirroring strips, all the "special" stuff happens just in setup.  We
@@ -41,26 +41,25 @@ void setup() {
 }
 
 void loop() {
-  /*
+  
     uint16_t beatA = beatsin16(35, 0, 255);
     uint16_t beatB = beatsin16(30, 0, 255);
     uint16_t beatC = beatsin16(25, 0, 255);
     uint16_t beatD = beatsin16(20, 0, 255);
-  */
 
   /*
     uint16_t beatA = beat8(35);
     uint16_t beatB = beat8(30);
     uint16_t beatC = beat8(25);
     uint16_t beatD = beat8(20);
-
+  */
     fill_rainbow(arm1Leds, NUM_LEDS_PER_STRIP, (beatA+beatB)/2, 8);
     fill_rainbow(arm2Leds, NUM_LEDS_PER_STRIP, (beatB+beatC)/2, 8);
     fill_rainbow(arm3Leds, NUM_LEDS_PER_STRIP, (beatC+beatD)/2, 8);
     fill_rainbow(arm4Leds, NUM_LEDS_PER_STRIP, (beatD+beatA)/2, 8);
-  */
+  
 
-    fire.DrawFire();
+    //fire.DrawFire();
     FastLED.show();
 
     EVERY_N_SECONDS(1) {
