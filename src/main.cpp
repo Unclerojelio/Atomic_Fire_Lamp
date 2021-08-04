7de367aaf89137992fb17de5bace99f012c10f3a
Roger Banks <roger_banks@mac.com>
2021-08-03 21:47:07 -0500

    Comment out Rainbow_Wobble and add code for Rainbow_Crawl.

--- src/main.cpp
+++ src/main.cpp
@@ -41,32 +41,40 @@ void setup() {
 }
 
 void loop() {
-  
-    uint16_t beatA = beatsin16(35, 0, 255);
-    uint16_t beatB = beatsin16(30, 0, 255);
-    uint16_t beatC = beatsin16(25, 0, 255);
-    uint16_t beatD = beatsin16(20, 0, 255);
-
   /*
-    uint16_t beatA = beat8(35);
-    uint16_t beatB = beat8(30);
-    uint16_t beatC = beat8(25);
-    uint16_t beatD = beat8(20);
+  // Rainbow Wobble
+  uint16_t beatA = beatsin16(35, 0, 255);
+  uint16_t beatB = beatsin16(30, 0, 255);
+  uint16_t beatC = beatsin16(25, 0, 255);
+  uint16_t beatD = beatsin16(20, 0, 255);
+
+  fill_rainbow(arm1Leds, NUM_LEDS_PER_STRIP, (beatA+beatB)/2, 8);
+  fill_rainbow(arm2Leds, NUM_LEDS_PER_STRIP, (beatB+beatC)/2, 8);
+  fill_rainbow(arm3Leds, NUM_LEDS_PER_STRIP, (beatC+beatD)/2, 8);
+  fill_rainbow(arm4Leds, NUM_LEDS_PER_STRIP, (beatD+beatA)/2, 8);
+  // Rainbow Wobble
   */
-    fill_rainbow(arm1Leds, NUM_LEDS_PER_STRIP, (beatA+beatB)/2, 8);
-    fill_rainbow(arm2Leds, NUM_LEDS_PER_STRIP, (beatB+beatC)/2, 8);
-    fill_rainbow(arm3Leds, NUM_LEDS_PER_STRIP, (beatC+beatD)/2, 8);
-    fill_rainbow(arm4Leds, NUM_LEDS_PER_STRIP, (beatD+beatA)/2, 8);
+
+  // Rainbow Crawl
+  uint16_t beatA = beat8(35);
+  uint16_t beatB = beat8(30);
+  uint16_t beatC = beat8(25);
+  uint16_t beatD = beat8(20);
   
+  fill_rainbow(arm1Leds, NUM_LEDS_PER_STRIP, beatA, 8);
+  fill_rainbow(arm2Leds, NUM_LEDS_PER_STRIP, beatB, 8);
+  fill_rainbow(arm3Leds, NUM_LEDS_PER_STRIP, beatC, 8);
+  fill_rainbow(arm4Leds, NUM_LEDS_PER_STRIP, beatD, 8);
+  // Rainbow Crawl
 
-    //fire1.DrawFire();
-    //fire2.DrawFire();
-    //fire3.DrawFire();
-    //fire4.DrawFire();
-    FastLED.show();
+  //fire1.DrawFire();
+  //fire2.DrawFire();
+  //fire3.DrawFire();
+  //fire4.DrawFire();
+  FastLED.show();
 
-    EVERY_N_SECONDS(1) {
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    }
-    delay(33);
+  EVERY_N_SECONDS(1) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
+  FastLED.delay(33);
 }
